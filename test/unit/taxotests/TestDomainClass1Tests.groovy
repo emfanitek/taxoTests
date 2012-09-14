cdfa338b0d769ddec300f938854a5c8f44f459ed
Luis Muniz <luis@emfanitek.com>
2012-09-14 16:00:13 +0200

    missing test class

---
+++ test/unit/taxotests/TestDomainClass1Tests.groovy
@@ -0,0 +1,18 @@
+package taxotests
+
+
+
+import grails.test.mixin.*
+
+import taxotests.testobjects.TestDomainClass1
+
+/**
+ * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
+ */
+@TestFor(TestDomainClass1)
+class TestDomainClass1Tests {
+
+    void testSomething() {
+       fail "Implement me"
+    }
+}
